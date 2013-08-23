3d97c3071762a36367f5435e04245f7f4ecbf36f
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2013-08-23 16:45:49 +0100

    Checking if a module has already been run or not.

--- piprun.py
+++ piprun.py
@@ -1,10 +1,39 @@
 
+import os.path
 import subprocess
 
+INSTALLED = """Flask
+itsdangerous
+Jinja2
+MarkupSafe
+pip
+python-dateutil
+pytz
+redis
+rq
+rq-dashboard
+setuptools
+simplejson
+six
+times
+Werkzeug
+wheel
+wsgiref""".split('\n')
+
+
 def process(package_name):
-    subprocess.call(['pip', 'install', '--no-deps', package_name])
+    filename = 'data/%s.bin' % package_name
+
+    if os.path.exists(filename):
+        return
+
+    if package_name not in INSTALLED:
+        subprocess.call(['pip', 'install', '--no-deps', package_name])
+
     subprocess.call(['python', 'saveinfo.py', package_name])
-    subprocess.call(['pip', 'uninstall', '-y', package_name])
+
+    if package_name not in INSTALLED:
+        subprocess.call(['pip', 'uninstall', '-y', package_name])
 
 
 

9390363004fdfd4cfdfaeaa3c7cd61f3d5d587fe
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2013-08-23 16:27:39 +0100

    Made the install file easily editable

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
-PACKAGES="python-dev libxml2-dev libxslt-dev libsqlite3-dev libpq-dev libmysqlclient18 mysql-common libevent-dev libzmq-dev libmemcached-dev libssl1.0.0 libc6 swig libfreetype6 libfreetype6-dev libpng12-0 libpng12-dev tk8.5 tcl8.5 libqt4-gui"
+apt-get install build-essential \
+                python-dev \
+                libxml2-dev \
+                libxslt-dev \
+                libsqlite3-dev \
+                libpq-dev \
+                libmysqlclient18 \
+                mysql-common \
+                libevent-dev \
+                libzmq-dev \
+                libmemcached-dev \
+                libssl1.0.0 \
+                libc6 \
+                swig \
+                libfreetype6 \
+                libfreetype6-dev \
+                libpng12-0 \
+                libpng12-dev \
+                tk8.5 \
+                tcl8.5 \
+                libqt4-gui
 
-apt-get install $PACKAGES

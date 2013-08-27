ed37a1778926e979d7681cfe2d80056e8d5e7e64
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2013-08-27 10:25:05 +0100

    Made the pip data extractor run in a separate virtual environment

--- piprun.py
+++ piprun.py
@@ -8,10 +8,17 @@ def process(package_name):
     if fileinfo.exists(package_name):
         return
 
-    subprocess.call(['pip', 'install', '--no-deps', package_name])
-
-    subprocess.call(['python', 'extractdatapip.py', package_name])
-
+    package_name = 'numpy'
+    env_name = '%senv' % (package_name, )
+    cmds = ['virtualenv envs/%s' % env_name,
+        'source envs/%s/bin/activate' % env_name,
+        'pip install --no-deps %s' % package_name,
+        'python extractdatapip.py %s' % package_name,
+        'deactivate',
+        'rm -r envs/%s' % (env_name, )
+        ]
+    cmd = ' && '.join(cmds)
+    subprocess.call(cmd, shell=True)
 
 
 if __name__ == '__main__':
